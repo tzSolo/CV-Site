f7cc705dad5841892c193f5065be001eac8a5ae2
tzSolo <t0504114734@gmail.com>
2025-05-18 15:03:36 +0300

    Initial commit.

---
+++ Service/GitHubService.cs
@@ -0,0 +1,15 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class GitHubService
+    {
+        private readonly GitHubClient _client = new GitHubClient(new ProductHeaderValue("cv-site"));
+
+    }
+}
